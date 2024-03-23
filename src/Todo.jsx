2d73e8323783075717108a80af9dd752a1e89d9f
surender77180 <surender791999@gmail.com>
2024-03-23 17:36:06 +0530

    mul.. value store

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,21 +1,26 @@
- import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import React from 'react'
 import { useState } from 'react';
 
 export default function Todo() {
 
-        let [cat, setCat] = useState()
-        let [data, setData] = useState([])
+  let [cat, setCat] = useState()
+  let [data, setData] = useState([])
 
-    return (
-        <>
-        <h1 className="m-4" >Todo-List</h1>
-        <form onSubmit={(e)=>e.preventDefault()}>
-        <input type='text' value={cat} name='cname' onChange={(eve) =>setCat(eve.target.value)} placeholder='Add dictionary' className="p-1 m-4" />
-        <Button variant="outline-success" onClick={()=>setData(cat)} className='mb-1' >Add</Button>
-        <h1>{[...data]}</h1>
-        </form>
-        </>
+  return (
+    <>
+      <h1 className="m-4" >Todo-List</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type='text' value={cat} name='cname' onChange={(eve) => setCat(eve.target.value)} placeholder='Add dictionary' className="p-1 m-4" />
+        <Button variant="outline-success" onClick={() => setData([...data, cat])} className='mb-1' >Add</Button>
+
+        {data.map((dt)=>{
+          return (<h1>{dt}</h1>)
+        })}
+       
+
+      </form>
+    </>
 
   )
 }
